c38cb1901b5ad2a410ac60f72fa93c34392032c1
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2021-02-12 18:21:50 +0100

    capitalized English words

--- src/data.js
+++ src/data.js
@@ -2,61 +2,61 @@ export const data = {
   words: [
     {
       german: "Der Vater",
-      english: "father",
+      english: "Father",
       gender: "male",
       id: "1"
     },
     {
       german: "Die Mutter",
-      english: "mother",
+      english: "Mother",
       gender: "female",
       id: "2"
     },
     {
       german: "Eltern",
-      english: "parents",
+      english: "Parents",
       gender: null,
       id: "3"
     },
     {
       german: "Der Sohn",
-      english: "son",
+      english: "Son",
       gender: "male",
       id: "4"
     },
     {
       german: "Die Tochter",
-      english: "daughter",
+      english: "Daughter",
       gender: "female",
       id: "5"
     },
     {
       german: "Die Enkelin",
-      english: "granddaughter",
+      english: "Granddaughter",
       gender: "female",
       id: "6"
     },
     {
       german: "Die Enkel",
-      english: "grandson",
+      english: "Grandson",
       gender: "female",
       id: "7"
     },
     {
       german: "Der Bruder",
-      english: "brother",
+      english: "Brother",
       gender: "male",
       id: "8"
     },
     {
       german: "Die Schwester",
-      english: "sister",
+      english: "Sister",
       gender: "female",
       id: "9"
     },
     {
       german: "Geschwister",
-      english: "siblings",
+      english: "Siblings",
       gender: null,
       id: "10"
     },
